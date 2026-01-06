5a1b0bb527e2fb5f7b5bb2683422a56c86dd6a8c
Tau <monkey3659@gmail.com>
2026-01-06 21:21:10 +0800

    Update simple_server.js

--- simple_server.js
+++ simple_server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // 静态文件服务 - 提供游戏文件
 app.use(express.static(path.join(__dirname)));
